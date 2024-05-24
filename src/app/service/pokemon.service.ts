7c395d6cba302a25c9fb49df76b7793fe0de7601
Joao felipe <jaofelpe14@gmail.com>
2024-05-24 16:16:56 -0300

    feat: :art: Cosumindo Api do pokemon
    Cosumindo Api do pokemon

---
+++ src/app/service/pokemon.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from "rxjs/operators";
+import { PokemonDetail, PokemonList } from '../model/pokemon.model';
+
+@Injectable({ providedIn: 'root' })
+export class PokemonService {
+  private baseUrl = 'https://pokeapi.co/api/v2/';
+
+  constructor(private http: HttpClient) { }
+
+  getPokemonList(offset: number, limit: number = 15): Observable<PokemonList[]> {
+    return this.http.get<any>(`${this.baseUrl}pokemon?offset=${offset}&limit=${limit}`)
+      .pipe(
+        map(response => response.results),
+        catchError(error => this.handleError(error))
+      );
+  }
+
+  getPokemonDetail(name: string): Observable<PokemonDetail> {
+    return this.http.get<PokemonDetail>(`${this.baseUrl}pokemon/${name}`)
+      .pipe(
+        map(pokemon => pokemon),
+        catchError(error => this.handleError(error))
+      );
+  }
+  
+  getPokemonListByType(typeName: string): Observable<PokemonList[]> {
+    return this.http.get<any>(`${this.baseUrl}type/${typeName}`)
+      .pipe(
+        map(response => response.pokemon.map((item: any) => item.pokemon)),
+        catchError(error => this.handleError(error))
+      );
+  }
+
+  getAllPokemonNames(): Observable<PokemonList[]> {
+    return this.http.get<any>(`${this.baseUrl}pokemon?limit=9999`)
+      .pipe(
+        map(response => response.results.map((pokemon: PokemonList) => pokemon)),
+        catchError(error => this.handleError(error))
+      );
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.error('Ocorreu um erro:', error);
+    return throwError('Erro ocorreu na solicitação. Por favor, tente novamente mais tarde.');
+  }
+}
